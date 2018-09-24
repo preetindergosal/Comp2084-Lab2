f21c3c24029dde5a9a62167587ae4e9b66a4a94b
Preetinder Singh Gosal <preetindergosal98@gmail.com>
2018-09-24 15:26:36 -0400

    Initial commit

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+
+        }
+    }
+}
